9cba6edade3d0222120bbf07d520ead5d9f349d3
Balaji Saravanan <balajilitsv@gmail.com>
2020-06-06 23:26:46 +0530

    Left panel creation

--- README.md
+++
@@ -1,4 +0,0 @@
-# personal-portofolio
-A personal portofolio describing myself
-
-Testing weather git commit is working.
---
+++ balaji-portofolio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="app">
+		<LeftNavigation/>
+		<div></div>
+		<div></div>
+	</div>
+</template>
+
+<script>
+import LeftNavigation from './components/LeftNavigation.vue'
+export default {
+  components: {
+		LeftNavigation
+	}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+	display: flex;
+	flex-direction: row;
+}
+</style>
